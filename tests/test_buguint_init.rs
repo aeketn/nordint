b5dab0e3de59a9253b3916facf9524d3fa348da4
Erik <aeketn@gmail.com>
2018-07-14 19:00:59 -0700

    BigUint FromStr Initialization
    Added FromStr initialization to the BigUint struct
    
    ======> lib.rs <======
    - added ParseBigIntError
    - added BigIntErrorKind
    - impl __description() for ParseBigIntError
    - impl empty() for ParseBigIntError
    - impl invalid() for ParseBigIntError
    - impl fmt() for ParseBigIntError
    - impl description() for ParseBigIntError
    ======> biguint.rs <======
    - impl new() for BigUint
    - impl from_str() for BigUint
    ======> test_biguint_init.rs <======
    - add test new() for BigUint
    - added test from_str() for BigUint

--- tests/test_buguint_init.rs
+++ tests/test_buguint_init.rs
@@ -2,6 +2,7 @@ extern crate nordint;
 
 mod tests {
     use nordint::BigUint;
+    use std::str::FromStr;
 
     #[test]
     fn biguint_empty() {
@@ -33,6 +34,121 @@ mod tests {
     fn biguint_default() {
         let number = BigUint::default();
         assert_eq!("BigUint { buckets: [] }", format!("{:?}", number));
+    }
+
+    #[test]
+    fn biguint_new() {
+        let number = BigUint::new("");
+        assert_eq!("BigUint { buckets: [] }", format!("{:?}", number));
+
+        let number = BigUint::new("123456789000");
+        assert_eq!(
+            "BigUint { buckets: [123456789000] }",
+            format!("{:?}", number)
+        );
+
+        let number = BigUint::new("123_456_789_000");
+        assert_eq!(
+            "BigUint { buckets: [123456789000] }",
+            format!("{:?}", number)
+        );
+
+        let number = BigUint::new("123,456,789,000");
+        assert_eq!(
+            "BigUint { buckets: [123456789000] }",
+            format!("{:?}", number)
+        );
+
+        let number = BigUint::new("000123456789000");
+        assert_eq!(
+            "BigUint { buckets: [123456789000] }",
+            format!("{:?}", number)
+        );
+
+        let number = BigUint::new("00000000000000000000000000123456789000");
+        assert_eq!(
+            "BigUint { buckets: [123456789000] }",
+            format!("{:?}", number)
+        );
+
+        let number = BigUint::new("123456789123456789_123456789123456789");
+        assert_eq!(
+            "BigUint { buckets: [123456789123456789, 123456789123456789] }",
+            format!("{:?}", number)
+        );
+
+        let number = BigUint::new("123456789123456789_023456789023456789_000000000000000001");
+        assert_eq!(
+            "BigUint { buckets: [1, 23456789023456789, 123456789123456789] }",
+            format!("{:?}", number)
+        );
+
+        let number = BigUint::new("100000000000000000_000000000000000000_000000000000000001");
+        assert_eq!(
+            "BigUint { buckets: [1, 0, 100000000000000000] }",
+            format!("{:?}", number)
+        );
+
+        let number = BigUint::new("1_000000000000000000_000000000000000000_000000000000000011");
+        assert_eq!(
+            "BigUint { buckets: [11, 0, 0, 1] }",
+            format!("{:?}", number)
+        );
+    }
+
+    #[test]
+    fn biguint_from_str() {
+        let number = BigUint::from_str("").unwrap_or_default();
+        assert_eq!(format!("{:?}", BigUint::default()), format!("{:?}", number));
+
+        let number = BigUint::from_str("123,456,789,000").unwrap_or_default();
+        assert_eq!(format!("{:?}", BigUint::default()), format!("{:?}", number));
+
+        let number = BigUint::from_str("123456789000").unwrap_or_default();
+        assert_eq!(
+            "BigUint { buckets: [123456789000] }",
+            format!("{:?}", number)
+        );
+
+        let number = BigUint::from_str("000123456789000").unwrap_or_default();
+        assert_eq!(
+            "BigUint { buckets: [123456789000] }",
+            format!("{:?}", number)
+        );
+
+        let number =
+            BigUint::from_str("00000000000000000000000000123456789000").unwrap_or_default();
+        assert_eq!(
+            "BigUint { buckets: [123456789000] }",
+            format!("{:?}", number)
+        );
+
+        let number = BigUint::from_str("123456789123456789123456789123456789").unwrap_or_default();
+        assert_eq!(
+            "BigUint { buckets: [123456789123456789, 123456789123456789] }",
+            format!("{:?}", number)
+        );
+
+        let number = BigUint::from_str("123456789123456789023456789023456789000000000000000001")
+            .unwrap_or_default();
+        assert_eq!(
+            "BigUint { buckets: [1, 23456789023456789, 123456789123456789] }",
+            format!("{:?}", number)
+        );
+
+        let number = BigUint::from_str("100000000000000000000000000000000000000000000000000001")
+            .unwrap_or_default();
+        assert_eq!(
+            "BigUint { buckets: [1, 0, 100000000000000000] }",
+            format!("{:?}", number)
+        );
+
+        let number = BigUint::from_str("1000000000000000000000000000000000000000000000000000011")
+            .unwrap_or_default();
+        assert_eq!(
+            "BigUint { buckets: [11, 0, 0, 1] }",
+            format!("{:?}", number)
+        );
     }
 
 }
